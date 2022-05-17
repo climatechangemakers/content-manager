c5671e3a33e359d7d2685fbf311e94e255cf323a
Nicholas Rutowski <nicholasrutowski@Nicholass-MacBook-Pro.local>
2022-05-17 19:41:44 -0400

    initial setup of strapi

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f7cc579cba518a4978b03f733e3b3dde'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8899b8f2-0aa4-4fc1-a356-5c210dd3e7df'
+};
